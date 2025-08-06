71a15c9c2af508ad6eb79ab0e1fd9a12d43425d6
thestrangemonster <thomas.gretten@live.fr>
2025-08-06 14:13:10 +0200

    test insomnia with endpoint post

---
+++ testApiWithIsomnia.md
@@ -0,0 +1,22 @@
+header
+Content-Type: application/json
+Accept: application/json
+
+body => json
+{
+    "name_created": "Mojito Magique",
+    "ingredients": "Rhum blanc, menthe fraîche, citron vert, sucre de canne, eau gazeuse",
+    "story_describe": "Un cocktail cubain revisité avec une touche de magie. Parfait pour les soirées d'été.",
+    "sound_ambiance": "Salsa cubaine relaxante",
+    "picture_prompt": "mojito with sparkling mint leaves and magical glow",
+    "cocktail_prompt": "cocktail rafraîchissant à la menthe pour l'été"
+}
+
+{
+    "name_created": "Cosmopolitan Stellaire",
+    "ingredients": "Vodka premium, liqueur de cranberry, jus de lime, triple sec",
+    "story_describe": "Une version cosmique du célèbre Cosmopolitan.",
+    "sound_ambiance": "Jazz moderne sophistiqué",
+    "picture_prompt": "pink cosmopolitan with stars",
+    "cocktail_prompt": "cocktail élégant et fruité"
+}
--- view/cocktails.py
+++ view/cocktails.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, render_template, jsonify, request
 from models import db, StockCocktails
 
 cocktails_bp = Blueprint('cocktails', __name__)
@@ -15,3 +15,18 @@ def get_cocktails():
         'picture_prompt': cocktail.picture_prompt,
         'cocktail_prompt': cocktail.cocktail_prompt
     } for cocktail in cocktails])
+
+@cocktails_bp.route('/api/cocktails', methods=['POST'])
+def add_cocktail():
+    data = request.get_json()
+    new_cocktail = StockCocktails(
+        name_created=data['name_created'],
+        ingredients=data['ingredients'],
+        story_describe=data['story_describe'],
+        sound_ambiance=data['sound_ambiance'],
+        picture_prompt=data['picture_prompt'],
+        cocktail_prompt=data['cocktail_prompt']
+    )
+    db.session.add(new_cocktail)
+    db.session.commit()
+    return jsonify({'message': 'Cocktail added successfully!'}), 201
