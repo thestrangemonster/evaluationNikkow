a9fb365c2e1282746f19bccef9b2906e74cf8b15
thestrangemonster <thomas.gretten@live.fr>
2025-08-06 15:09:03 +0200

    modification of app.py for docker

--- app.py
+++ app.py
@@ -3,12 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from view.main import main_bp
 from view.cocktails import cocktails_bp
 from models import db
+import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bar_cocktails.db'
+# Configuration de l'application pour Docker
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+    'DATABASE_URL', 
+    'sqlite:///bar_cocktails.db')
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'your-secret-key-here'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'your_secret_key')
 
 db.init_app(app)
 
@@ -19,9 +24,10 @@ app.register_blueprint(cocktails_bp)
 
 if __name__ == '__main__':
     with app.app_context():
+        os.makedirs('data', exist_ok=True)  # Crée le dossier data s'il n'existe pas
         db.create_all()  # Crée les tables dans la base de données si elles n'existent pas
     # Démarrer l'application Flask  
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000) # Exécute l'application sur toutes les interfaces réseau pour Docker
 
 
 
