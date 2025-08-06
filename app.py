14bb14aec547f72fa070edb1eef2ebc65ed493b4
thestrangemonster <thomas.gretten@live.fr>
2025-08-06 10:54:27 +0200

    fix app.py

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from view.main import main_bp
+from models import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bar_cocktails.db'
+
+db.init_app(app)
+
+# Enregistrement des Blueprints
+app.register_blueprint(main_bp)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
