0bbc48032b61e392a57f4c49ad3079085218f70f
thestrangemonster <thomas.gretten@live.fr>
2025-08-06 10:42:00 +0200

    add templates and cdn bootstrap

--- models/stock_cocktails.py
+++ models/stock_cocktails.py
@@ -9,3 +9,4 @@ class StockCocktails(db.Model):
     story_describe = db.Column(db.Text, nullable=False)
     sound_ambiance = db.Column(db.String(100), nullable=False)
     picture_prompt = db.Column(db.String(100), nullable=False)
+    cocktail_prompt = db.Column(db.String(100), nullable=False)
