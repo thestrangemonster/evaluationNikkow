9ad1d8a3482beb9500a977aa09db03a2d552ff62
thestrangemonster <thomas.gretten@live.fr>
2025-08-06 09:49:02 +0200

    update database stockCocktails

--- models/stock_cocktails.py
+++ models/stock_cocktails.py
@@ -4,4 +4,8 @@ class StockCocktails(db.Model):
     __tablename__ = 'stock_cocktails'
 
     id = db.Column(db.Integer, primary_key=True)
-       
+    name_created = db.Column(db.String(100), nullable=False)
+    ingredients = db.Column(db.Text, nullable=False)
+    story_describe = db.Column(db.Text, nullable=False)
+    sound_ambiance = db.Column(db.String(100), nullable=False)
+    picture_prompt = db.Column(db.String(100), nullable=False)
